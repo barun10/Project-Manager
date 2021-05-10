d11dfa06f27bd0e1974bf998234a4a8a66e4d66b
Barunabha Poddar <barun1996poddar@gmail.com>
2021-05-10 09:56:58 +0530

    Adding fake datas

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  User.create(
+    name: Faker::Name.name,   
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end 
+
+15.times do
+  Project.create(
+    name: Faker::Name.name,
+    user_id: rand(1..User.count)
+  )
+end
+
+50.times do
+  Feature.create(
+    title: Faker::Name.name,
+    description: Faker::Lorem.paragraph,
+    ticket_id: Faker::Bank.account_number(digits: 8),
+    panel_name: 'current' || 'icebox' || 'backlog',
+    status: 'completed' || 'started' || 'delivered' || 'unstarted',
+    project_id: rand(1..Project.count)
+  )
+end
+
+100.times do
+  Task.create(
+    description: Faker::Lorem.paragraph,
+    status: false,
+    feature_id: rand(1..Feature.count)
+  )
+end
+
+100.times do
+  Comment.create(
+    comment: Faker::Lorem.paragraph,
+    feature_id: rand(1..Feature.count),
+    user_id: rand(1..User.count)
+  )
+end
